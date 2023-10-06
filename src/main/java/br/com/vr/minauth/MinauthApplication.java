857561628b428236f843b81959baf4694fdd5edc
Pedro Natan <pedronatan.tm@gmail.com>
2023-10-06 10:45:14 -0300

    Initial structure and imports

---
+++ src/main/java/br/com/vr/minauth/MinauthApplication.java
@@ -0,0 +1,13 @@
+package br.com.vr.minauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinauthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vr/minauth/MinauthApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vr.minauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
